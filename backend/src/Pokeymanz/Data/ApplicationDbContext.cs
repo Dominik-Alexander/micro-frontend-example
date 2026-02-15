d5145408be9aec434ef1f138aef4e07cef652e23
PeanutLager <56730480+Dominik-Alexander@users.noreply.github.com>
2026-02-15 22:37:35 +0100

    Feature/add backend (#1)
    Add a very rudimentary backend to play around
    
    * set up ASP.NET
    * set up postgres
    * add entities as examples

---
+++ backend/src/Pokeymanz/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pokeymanz.Models.Entities;
+
+namespace Pokeymanz.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options) 
+        {
+        
+        }
+
+        public DbSet<Pokeyman> Pokeymanz { get; set; }
+    }
+}
