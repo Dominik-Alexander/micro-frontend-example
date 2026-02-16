4542f49f6b1e45856ffc5104433910c2dcdf9a63
Dominik Szczepek <DSzczepek@outlook.de>
2026-02-17 00:34:33 +0100

    Replace button with prime button

--- frontend/remote-app/src/components/Button.tsx
+++ frontend/remote-app/src/components/Button.tsx
@@ -1,5 +1,6 @@
 // remote - ./src/components/Button.tsx
 import React from "react";
+import { Button as PrimeButton } from 'primereact/button'
 
 interface ButtonProps {
     text: string;
@@ -8,12 +9,9 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
     return (
-        <button
-            onClick={onClick}
-            className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 hover:cursor-pointer"
-        >
-            {text}
-        </button>
+        <PrimeButton severity="success" label={text}  
+        className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 hover:cursor-pointer" onClick={onClick}>
+        </PrimeButton>
     );
 };
 
